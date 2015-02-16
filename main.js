c68b39f09ae607a46b2b4f9415d763e7005263df
Becky Hamm <becky@metricslice.com>
2015-02-16 16:30:55 -0500

    finished part II

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ var buildTree = function(data) {
         $rootLi = $("<li />");
         $rootLi.append("<span>" + lvl1Category.name + "</span>");
 
-
-        if (lvl1Category.children) { // computers, home entertainment, fiction
+        // START LEVEL 2 computers, home entertainment, fiction
+        if (lvl1Category.children) { 
             lvl2Data = lvl1Category.children;
             //console.log(lvl2Data);
             $lvl2Ul = $("<ul />");
@@ -39,24 +39,58 @@ var buildTree = function(data) {
                 $lvl2Li = $("<li />");
                 $lvl2Li.append("<span>" + lvl2Category.name + "</span>");
 
-
+                // START LEVEL 3
                 if (lvl2Category.children) {
                     lvl3Data = lvl2Category.children;
                     //console.log(lvl3Data);
                     $lvl3Ul = $("<ul />");
 
                     _.each(lvl3Data, function(lvl3Category) {
+                        var $lvl4Ul, $lvl4Li, lvl4Data;
                         $lvl3Li = $("<li />");
                         $lvl3Li.append("<span>" + lvl3Category.name + "</span>");
                         //console.log($lvl3Li[0]);
+
+                        // START LEVEL 4
+                        if (lvl3Category.children) {
+                            lvl4Data = lvl3Category.children;
+                            $lvl4Ul = $("<ul />");
+
+                            _.each(lvl4Data, function(lvl4Category){
+                                var $lvl5Ul, $lvl5Li, lvl5Data;
+                                $lvl4Li = $("<li />");
+                                $lvl4Li.append("<span>" + lvl4Category.name + "</span>");
+
+                                if(lvl4Category.children) {
+                                    lvl5Data = lvl4Category.children;
+                                    $lvl5Ul = $("<ul />");
+
+                                    _.each(lvl5Data, function(lvl5Category) {
+                                        $lvl5Li = $("<li />");
+                                        $lvl5Li.append("<span>" + lvl5Category.name + "</span>");
+                                        $lvl5Ul.append($lvl5Li);
+                                    });
+
+                                    $lvl4Li.append($lvl5Ul);
+                                }
+
+
+                                $lvl4Ul.append($lvl4Li);
+
+                            });
+
+                            $lvl3Li.append($lvl4Ul);
+
+                        } // END LEVEL 4
+
+
+
                         $lvl3Ul.append($lvl3Li);
                     });
 
                     $lvl2Li.append($lvl3Ul);
 
-                } // end level 3: macbook air, hdtvs
-
-
+                } // END LEVEL 3: macbook air, hdtvs
 
                 $lvl2Ul.append($lvl2Li);
             });
@@ -64,7 +98,7 @@ var buildTree = function(data) {
             // append the level 2 ul to the level 1 li
             $rootLi.append($lvl2Ul);
 
-        } // end if there are lvl1 chilcren
+        } // END LEVEL 2
 
         // append root li to root ul
         $rootUl.append($rootLi);
